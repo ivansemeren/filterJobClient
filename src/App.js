6902f0077f7cb917de5dbe19eb90cac259b84f7f
ivansemeren <ivankosemerenko@gmail.com>
2020-04-14 19:14:27 -0400

    SearchingReady

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Jobs from './Jobs'
+import fetch from 'node-fetch';
+
+const JOB_API_URL = 'http://localhost:3001/jobs';
+
+// const mockJobs = [
+//   {title: 'SWE 1', company: 'Google', },
+//   {title: 'SWE 2', company: 'Amazon', },
+//   {title: 'SWE 2', company: 'Facebook', }
+// ]
+
+async function fetchJobs(updateCb) {
+  const res = await fetch(JOB_API_URL);
+  const json = await res.json();
+
+  updateCb(json);
+
+  //console.log({json});
+}
 
 function App() {
+
+  const [jobList, updateJobs] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchJobs(updateJobs);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jobs jobs={jobList}/>
     </div>
   );
 }
+
+
 
 export default App;
